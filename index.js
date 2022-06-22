a24f04bff97c1318a0cb1ff9fe996c7597ae2a4a
BullishVince <vincent@bullishlife.com>
2022-06-22 22:05:28 +0200

    add pkgs + create file structure

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//add routes
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+//start server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
